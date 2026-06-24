206e351838c65b7edac9e73d48c255618542f148
jea baluga <jeabaluga@gmail.com>
2026-06-24 16:10:03 +0800

    jeabalug

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Operations</title>
-    <link   rel="stylesheet" href="style.css">
+    <title>'CRUD Operations'</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <nav class="navbar">
@@ -22,42 +23,89 @@
     <section id="create" class="content">
         <h1 class="contenttitle"> Insert New Student </h1>
 
-    <form action="../includes/insert.php" method="POST">
-        <label for="surname" class="label">Surname</label>
-        <input type="text" name="surname" id="surname" class="field" required><br/>
+    <div class="form-group">
+        <input id="surname" placeholder="Surname">
+        <input id="name" placeholder="Name">
+        <input id="middlename" placeholder="Middle Name">
+        <input id="address" placeholder="Address">
+        <input id="contact" placeholder="Contact">
+    </div>
 
-        <label for="name" class="label">Name</label>
-        <input type="text" name="name" id="name" class="field" required><br/>
+    <button onclick="addStudent()">Save</button>
+</section>
+<section id="read" class="content">
+      <h2>Student Records</h2>
 
-        <label for="middlename" class="label">Middle name</label>
-        <input type="text" name="middlename" id="middlename" class="field"><br/>
+    <table id="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Full Name</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+</section>
+ 
 
-        <label for="address" class="label">Address</label>
-        <input type="text" name="address" id="address" class="field"><br/>
+</div>
 
-        <label for="contact" class="label">Mobile Number</label>
-        <input type="text" name="contact" id="contact" class="field"><br/>
-
-        <div id="btncontainer">
-            <button type="button" id="clrbtn" class="btns">Clear Fields</button><br/>
-            <button type="submit" id="savebtn" class="btns">Save</button>
-        </div>
-
-        <div id="success-toast" class="toast-hidden">
-            Registration Successful!
-        </div>
-    </form>   
-
+<script src="script.js"></script>
+</body>
+</html><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>'CRUD Operations'</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav class="navbar">
+            <img src="../images/northhub.svg" id="logo"></img>
+            <button class="navbarbuttons" onclick="showSection('create')"> Create </button>
+            <button class="navbarbuttons" > Read </button>
+            <button class="navbarbuttons" > Update </button>
+            <button class="navbarbuttons" > Delete </button>
+    </nav>
+    <section id="home" class="homecontent"> 
+        <h1 class="splash">Welcome to Student Management System</h1>
+        <h2 class="splash">A Project in Integrative Programming Technologies</h2>
     </section>
+    
+    <section id="create" class="content">
+        <h1 class="contenttitle"> Insert New Student </h1>
 
-<br/><br/><br/><br/>
+    <div class="form-group">
+        <input id="surname" placeholder="Surname">
+        <input id="name" placeholder="Name">
+        <input id="middlename" placeholder="Middle Name">
+        <input id="address" placeholder="Address">
+        <input id="contact" placeholder="Contact">
+    </div>
 
-    <section id="read" class="content"> View Students </section>
-    <section id="update" class="content"> Update Student Records </section>
-    <section id="delete" class="content"> Remove Student Records </section>
+    <button onclick="addStudent()">Save</button>
+</section>
+<section id="read" class="content">
+      <h2>Student Records</h2>
 
+    <table id="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Full Name</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+</section>
+ 
 
+</div>
 
-    <script src="script.js"></script>
+<script src="script.js"></script>
 </body>
 </html>
