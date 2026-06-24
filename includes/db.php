7171be5447d2430470ffe284a1ab79adb0fb4424
jea baluga <jeabaluga@gmail.com>
2026-06-24 13:19:51 +0800

    jeabaluga

--- includes/db.php
+++ includes/db.php
@@ -1,25 +1,20 @@
 <?php
-$host = '127.0.0.1'; 
-$db   = 'cc111smsdb';  //sukatan yo dytoy jay nagan ti database yo
-$user = 'root';        //
-$pass = '';            //
-$port = '3308';        //nu nagusar kayo sabali nga port sukatan yo metlang dytoy
+$host    = '127.0.0.1';
+$dbname  = 'dbstudents';
+$user    = 'root';
+$pass    = '';
+$port    = '3306';
 $charset = 'utf8mb4';
-
-
-$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
 
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-
+    $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-
-     die("Connection failed: " . $e->getMessage());
+    die(json_encode(["status" => "error", "message" => "Connection failed: " . $e->getMessage()]));
 }
 ?>
